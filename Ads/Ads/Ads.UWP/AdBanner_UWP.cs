c35a5d719ae6b57fdc52bf37b6b5b9679bee033c
Pranav Sharma <pranavsharma@outlook.com.au>
2017-02-21 19:19:28 +1100

    UWP implementation

---
+++ Ads/Ads/Ads.UWP/AdBanner_UWP.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Ads;
+using Xamarin.Forms;
+using Ads.UWP;
+using Xamarin.Forms.Platform.UWP;
+using Microsoft.Advertising.WinRT.UI;
+using Windows.UI.Xaml;
+using Windows.System.Profile;
+
+[assembly: ExportRenderer(typeof(AdBanner), typeof(AdBanner_UWP))]
+namespace Ads.UWP
+{
+    public class AdBanner_UWP : ViewRenderer<View, FrameworkElement>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                AdControl bannerView = new AdControl();
+
+                switch ((Element as AdBanner).Size)
+                {
+                    case "Standardbanner":
+                        bannerView.Width = 320;
+                        bannerView.Height = 50;
+                        break;
+                    case "LargeBanner":
+                        bannerView.Width = 320;
+                        bannerView.Height = 50;
+                        break;
+                    case "MediumRectangle":
+                        bannerView.Width = 300;
+                        bannerView.Height = 250;
+                        break;
+                    case "FullBanner":
+                        bannerView.Width = 480;
+                        bannerView.Height = 80;
+                        break;
+                    case "Leaderboard":
+                        bannerView.Width = 728;
+                        bannerView.Height = 90;
+                        break;
+                    case "SmartBannerPortrait":
+                        bannerView.Width = 320;
+                        bannerView.Height = 50;
+                        break;
+                    default:
+                        bannerView.Width = 320;
+                        bannerView.Height = 50;
+                        break;
+                }
+
+                if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
+                {
+                    bannerView.ApplicationId = "3f83fe91-d6be-434d-a0ae-7351c5a997f1";
+                    bannerView.AdUnitId = "10865275";
+                }
+                else
+                {
+                    bannerView.ApplicationId = "3f83fe91-d6be-434d-a0ae-7351c5a997f1";
+                    bannerView.AdUnitId = "10865275";
+                }
+
+                if (bannerView != null)
+                    SetNativeControl(bannerView);
+            }
+        }
+    }
+}
---
+++ Ads/Ads/Ads.UWP/AdInterstitial_UWP.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Advertising.WinRT.UI;
+using Ads.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AdInterstitial_UWP))]
+namespace Ads.UWP
+{
+    class AdInterstitial_UWP : IAdInterstitial
+    {
+        InterstitialAd interstitialAd;
+        string applicationID = null;
+        string adUnitID = null;
+
+        public AdInterstitial_UWP()
+        {
+            interstitialAd = new InterstitialAd();
+
+            if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
+            {
+                applicationID = "d25517cb-12d4-4699-8bdc-52040c712cab";
+                adUnitID = "11389925";
+            }
+            else
+            {
+                applicationID = "d25517cb-12d4-4699-8bdc-52040c712cab";
+                adUnitID = "11389925";
+            }
+
+            interstitialAd.Completed += (s, e) => LoadAd();
+            LoadAd();
+        }
+
+        void LoadAd()
+        {
+            interstitialAd.RequestAd(AdType.Video, applicationID, adUnitID);
+        }
+
+        public void ShowAd()
+        {
+            if (InterstitialAdState.Ready == interstitialAd.State)
+                interstitialAd.Show();
+        }
+    }
+}
